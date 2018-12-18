43975177690abe60c6e95f942f5ea55e1c21602b
sergem155 <sergey.menshikov@gmail.com>
2018-12-17 20:54:35 -0800

    ensure utc, allow to select host, separate the list of known devices from the list of devices to query

--- config-example.py
+++ config-example.py
@@ -1,7 +1,9 @@
-hosts ={
+devices ={
 "A":"c4:7c:8d:66:ca:ad",
 "B":"C4:7C:8D:66:34:9E",
 "C":"C4:7C:8D:66:3F:EF"
 }
+
+to_scan = ["A","B"]
 
 influx_args=('localhost', 8086, 'root', 'pass', 'plant_monitors')

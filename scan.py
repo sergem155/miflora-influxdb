b7975f5fb8a67fac7c83cb5e8459975256364eae
sergem155 <sergey/menshikov@gmail.com>
2019-01-19 17:51:01 -0800

    scan bug fixes, supervision timeout script

--- scan.py
+++ scan.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral, BTLEException
 from config import *
 
 dev_dict = {}
@@ -13,24 +13,27 @@ class ScanDelegate(DefaultDelegate):
 	def handleDiscovery(self, dev, isNewDev, isNewData):
 		if dev.addr.upper().startswith('C4:7C:8D:'):
 			if isNewDev:
-				print("Discovered device: %s - %s" % (dev.addr,dev_dict[dev.addr.lower()]))
+				print("Discovered device: %s - %s" % (dev.addr,dev_dict.get(dev.addr.lower())))
 			elif isNewData:
-				print("Received new data from: %s - %s" % (dev.addr,dev_dict[dev.addr.lower()]))
+				print("Received new data from: %s - %s" % (dev.addr,dev_dict.get(dev.addr.lower())))
 
 scanner = Scanner().withDelegate(ScanDelegate())
 device_list = scanner.scan(10.0)
 
 for dev in device_list:
 	if dev.addr.upper().startswith('C4:7C:8D:'):
-		hostname = dev_dict[dev.addr.lower()]
+		hostname = dev_dict.get(dev.addr.lower())
 		print("Hostname: %s, Device %s (%s), RSSI=%d dB" % (hostname, dev.addr, dev.addrType, dev.rssi))
 		for (adtype, desc, value) in dev.getScanData():
 		    print("  %s = %s" % (desc, value))
-		device = Peripheral()
-		device.connect(dev.addr)
-		data = device.readCharacteristic(0x38)
-		battery = data[0]
-		fw = data[2:]
-		print(" - fw: %s, battery %d" %(fw, battery))
-		device.disconnect()
+		try:
+			device = Peripheral()
+			device.connect(dev.addr)
+			data = device.readCharacteristic(0x38)
+			battery = data[0]
+			fw = data[2:]
+			print(" - fw: %s, battery %d" %(fw, battery))
+			device.disconnect()
+		except BTLEException as e:
+			print("  Error connecting to the device")
 
